1cdc9591651cf7d9d5af5d315199571654c79c8d
Vaughn Walters <vaughnwalters@gmail.com>
2017-01-26 13:45:31 -0600

    made boilerpate code for AlbumList.js

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List!</Text>
+    </View>
+  );
+};
+
+export default AlbumList;
+
