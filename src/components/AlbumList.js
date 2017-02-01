b659bb1645ee6e690bfe9fc12730aeddd0328045
Vaughn Walters <vaughnwalters@gmail.com>
2017-01-31 19:27:35 -0600

    made AlbumDetail.js and passed in props from AlbumList.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 class AlbumList extends Component {
@@ -16,7 +17,7 @@ class AlbumList extends Component {
   renderAlbums() {
     // best to use for unique key is ID, but in lieu of that we can use the album.title here
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>);
+      <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
