48aeb5ee02fe7c4c33b1bb79b7602510aa4685bd
Vaughn Walters <vaughnwalters@gmail.com>
2017-01-31 18:27:55 -0600

    initialized state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 
 class AlbumList extends Component {
+  // initialize state
+  state = { albums: [] };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
   }
 
   render() {
