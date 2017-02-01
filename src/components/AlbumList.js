91c1e60c74e981bd24e670a5b83e5cabb70d22f5
Vaughn Walters <vaughnwalters@gmail.com>
2017-01-31 18:12:51 -0600

    added componentWillMount()

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,18 @@ import { View, Text } from 'react-native';
 
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
+
   render() {
     return (
       <View>
         <Text>Album List!</Text>
       </View>
     );
-  };  
-};
+  }  
+}
 
 export default AlbumList;
 
