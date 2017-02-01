0a812d7a4fa942bb1c31e5ec054bef643ee10b28
Vaughn Walters <vaughnwalters@gmail.com>
2017-01-31 19:14:58 -0600

    album titles rendering

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -9,13 +9,20 @@ class AlbumList extends Component {
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    // this.setState is the only way we will change the state
+    .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
   }
 
   render() {
+    console.log('this.state', this.state);
+
     return (
-      <View>
-        <Text>Album List!</Text>
+      <View> 
+        {this.renderAlbums()}
       </View>
     );
   }  
