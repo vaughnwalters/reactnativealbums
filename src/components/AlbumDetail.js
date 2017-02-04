015d255b0c6945b2cfb26b8dc9e4ed2967f8bdb8
Vaughn Walters <vaughnwalters@gmail.com>
2017-02-04 15:56:20 -0600

    album images showing up

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,16 +7,22 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={styles.thumbnailContainerStyle}>
           <Image 
             style={styles.thumbnailStyle}
             source={{ uri: props.album.thumbnail_image }} 
           />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
+          <Text style={styles.headerTextStyle}>{props.album.title}</Text>
           <Text>{props.album.artist}</Text>
         </View> 
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={styles.imageStyle}
+          source={{ uri: props.album.image }} 
+        />
       </CardSection>
     </Card>
   );
@@ -27,9 +33,22 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
